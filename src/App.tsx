10ad8bf6a45098a1c2677a3386e0fe77a7bd040d
long hao <owm30912@mitac.com.tw>
2024-08-05 19:22:25 +0800

    init react

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import Page from './component/Page'
+
+/*
+    1. useEffect的 effect(side-effect) 代表副作用, 用來處理與 react無關的事, ex: fetch、log...
+    2. useEffect的執行時機是 
+          I.  畫面初始化的時候 invoke -> in jsx -> useEffect
+          II. useEffect((callback(clean up function())), [dependencies]) 裡面的 dependencies value 有無改變
+    3. Promise
+
+
+
+
+
+*/
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  // useEffect(() => {
+  //   console.log("useEffect");
+  //   return () =>{
+  //     console.log("clean up");
+  //   };
+  // });
+
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     setCount(count + 1);
+  //     console.log("effect");
+  //   }, 500);
+  //   // clean up function
+  //   return () => {
+  //     console.log("clean up")
+  //     timer && clearInterval(timer);
+  //   }
+  // }, [count]);
+
+  // const addCount = () =>{
+  //   setCount(count + 1);
+  // }
+
+
+  // console.log("component render");
+
+  return (
+    <>
+      {/* <h1>count: {count}</h1>
+      {console.log("in jsx")} */}
+      {/* <button name="count" onClick={addCount}>add</button> */}
+      <Page />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Page.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import { fetchBio } from '../api';
+
+export default function Page() {
+  const [person, setPerson] = useState<string>('John');
+  const [bio, setBio] = useState<string | null>(null);
+  {
+    console.log('invoke');
+  }
+
+  useEffect(() => {
+    console.log('useEffect');
+    let ignore = false;
+    setBio(null);
+    fetchBio(person).then((result) => {
+      if (!ignore) {
+        setBio(result);
+      }
+    });
+    return () => {
+      console.log('clean up');
+      ignore = true;
+    };
+  }, [person]);
+
+  // const handleSelect = ({target}: React.ChangeEvent<HTMLSelectElement>) => {
+  //   setPerson(target.value);
+  // };
+
+  return (
+    <>
+      {console.log('in jsx')}
+      <select
+        value={person}
+        onChange={(event) => setPerson(event.target.value)}
+      >
+        <option value="John">John</option>
+        <option value="Amy">Amy</option>
+        <option value="Bob">Bob</option>
+      </select>
+      <hr />
+      <p>
+        <i>{bio ?? 'Loading'}</i>
+        <input
+          name="person"
+          onChange={(event) => {
+            setPerson(event.target.value);
+          }}
+        ></input>
+      </p>
+    </>
+  );
+}
