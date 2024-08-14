eb27f5a48747b2d0150af2c2787b4e772452dfca
long hao <owm30912@mitac.com.tw>
2024-08-14 21:30:24 +0800

    es config setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    // Note: you must disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn"
   },
 }
