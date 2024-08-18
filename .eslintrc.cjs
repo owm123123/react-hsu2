0a3bf74cd04dcd0101844f9d251696f44a3fa719
owm30911 <owm30911@gmail.com>
2024-08-18 16:56:24 +0800

    add eslintrc.cjs settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'prefer-const': 1,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
